bbc7f655c9f35186c06652829bf70f7f75b2aab6
Bator Ochirov <jdclay123@gmail.com>
2024-09-15 23:13:26 +0800

    added task11

--- src/chapter1/var1/RandomNumbers.java
+++ src/chapter1/var1/RandomNumbers.java
@@ -3,8 +3,6 @@ package chapter1.var1;
 import java.util.Random;
 import java.util.Scanner;
 
-import static java.lang.Math.random;
-
 public class RandomNumbers {
     public static void main(String[] args) {
 
